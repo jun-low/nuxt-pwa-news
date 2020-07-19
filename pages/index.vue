40c1154a17897fcea1bea29c9fed554a77c12f85
jun-low <zhijun.low@gmail.com>
2020-07-19 18:56:51 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,56 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-pwa-news
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div
+          class="column is-one-quarter"
+          v-for="(article, index) in articles"
+          :key="index">
+          <a :href="article.url" target="_blank">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-3by2">
+                  <img :src="article.urlToImage" :alt="article.title">
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content title is-6">{{ article.title }}</div>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
-<script>
-export default {}
+<script lang="js">
+export default {
+  async asyncData ({ app }) {
+    const { articles } = await app.$axios.$get(
+        `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${process.env.NEWS_API_KEY}`
+    );
+    return { articles };
+  },
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -66,7 +58,6 @@ export default {}
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
 }
